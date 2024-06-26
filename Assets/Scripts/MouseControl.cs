ae7a062036faaf763070a8223d6e0b6cbaa504b3
AneJlbcuH4uk <kovalchenko0511@gmail.com>
2024-06-26 16:27:57 +0300

    added basic UI and camera movement

--- Assets/Scripts/MouseControl.cs
+++ Assets/Scripts/MouseControl.cs
@@ -8,5 +8,64 @@ using UnityEngine.UI;
 
 public sealed class MouseControl : MonoBehaviour
 {
-  
+    [SerializeField] private PuzzlePiece holded_puzzle;
+
+    private static MouseControl instance;
+    private Collider2D force_field;
+
+
+    public static MouseControl GetInstance() 
+    {
+        if (instance == null) 
+        {
+            instance = new MouseControl();
+        }
+        return instance;
+    }
+
+   
+
+    private void Start()
+    {
+        force_field = gameObject.GetComponent<Collider2D>();
+        instance = this;
+    }
+
+    //private void Update()
+    //{
+    //    if (holded_puzzle != null)
+    //    {
+    //        holded_puzzle.CheckCollisionWithNeighbours();
+    //    }
+    //}
+
+    //private void FixedUpdate()
+    //{
+
+
+    //    //if (Input.GetButton("Fire2")) 
+    //    //{
+    //    //    var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    //    force_field.offset = mouseWorldPos;
+    //    //    force_field.enabled = true;
+
+    //    //}
+    //    //else
+    //    //    force_field.enabled = false;
+    //}
+
+    public void SetHoldedPuzzle(PuzzlePiece p) 
+    {
+        holded_puzzle = p;
+    }
+
+    public void UnsetHoldedPuzzle() 
+    {
+        holded_puzzle = null;
+    }
+
+    public bool Is_holding() 
+    {
+        return !System.Object.Equals(holded_puzzle, null);
+    }
 }
